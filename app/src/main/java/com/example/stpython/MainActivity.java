334aace385f4e6fa654180b65da28454d92b5ffb
Frances Fiona Labrador <127670176+piyunaaaaq@users.noreply.github.com>
2024-04-26 06:46:41 +0800

    log in

--- app/src/main/java/com/example/stpython/MainActivity.java
+++ app/src/main/java/com/example/stpython/MainActivity.java
@@ -2,37 +2,67 @@ package com.example.stpython;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    String [] items = {"Female","Male","Prefer not to say"};
+    String[] items = {"Female", "Male", "Prefer not to say"};
     AutoCompleteTextView autoCompleteTextView;
-
     ArrayAdapter<String> adapterItems;
+    EditText birthdate;
+    Calendar calendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        birthdate = findViewById(R.id.txtfieldbday);
+        calendar = Calendar.getInstance();
 
-        adapterItems = new  ArrayAdapter<String>(this,R.layout.dropdowngender_item,items);
-        autoCompleteTextView.setAdapter(adapterItems);
-
-        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-                String item  = parent.getItemAtPosition(position).toString();
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateCalendar();
+            }
+        };
+
+        birthdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
 
+        autoCompleteTextView = findViewById(R.id.autoCompleteTextView);
+        adapterItems = new ArrayAdapter<>(this, R.layout.dropdowngender_item, items);
+        autoCompleteTextView.setAdapter(adapterItems);
 
+        autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+            }
+        });
+    }
 
+    public void updateCalendar() {
+        String Format = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(Format, Locale.US);
+        birthdate.setText(sdf.format(calendar.getTime()));
     }
 }
